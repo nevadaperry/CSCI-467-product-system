fa0f58cb8f535ad7624bb4aeaa9f825303c26b27
GiddyTai <matthewgn123@gmail.com>
2023-08-08 00:14:50 -0500

    Added CC_func

--- frontend/src/Customer/CartPage.tsx
+++ frontend/src/Customer/CartPage.tsx
@@ -10,7 +10,17 @@ function CartPage({ cartItems, setCartItems, setTotalPrice, totalPrice }) {
   const [customerAddress, setCustomerAddress] = useState('');
   const [creditCardNumber, setCreditCardNumber] = useState('');
   const [orderAuthNumber, setOrderAuthNumber] = useState('');
-  const [orderCompleted, setOrderCompleted] = useState(false); 
+  const [orderCompleted, setOrderCompleted] = useState(false);
+
+  const [creditCardExp, setCreditCardExp] = useState('');
+  const [creditCardCVV, setCreditCardCVV] = useState('');
+  const [cardholderName, setCardholderName] = useState('');
+
+  
+  const [shippingAddress, setShippingAddress] = useState('');
+  const [useSameAddress, setUseSameAddress] = useState(false);
+
+   
 
 
   //TODO Finish the complete order function
@@ -27,6 +37,22 @@ function CartPage({ cartItems, setCartItems, setTotalPrice, totalPrice }) {
     setCreditCardNumber(newCreditCardNumber);
   };
 
+  const CreditCardExpChange = (e) => {
+  const newCreditCardExp = e.target.value;
+  setCreditCardExp(newCreditCardExp);
+};
+
+const CreditCardCVVChange = (e) => {
+  const newCreditCardCVV = e.target.value.replace(/\D/g, '').slice(0, 3); // Assuming CCV has 3 digits
+  setCreditCardCVV(newCreditCardCVV);
+};
+
+const CardholderNameChange = (e) => {
+  const newCardholderName = e.target.value;
+  setCardholderName(newCardholderName);
+};
+
+
   const comepleteOrder = async (event) => {
     console.log('Complete Order button clicked'); // Add this line
 
@@ -42,8 +68,11 @@ function CartPage({ cartItems, setCartItems, setTotalPrice, totalPrice }) {
         })),
         cc_full: {
           digits: creditCardNumber,
+          exp: creditCardExp, 
+          cvv: creditCardCVV,
+          cardholder_name: cardholderName,
         },
-        shipping_address: '1234 ginderbread ln,',
+        shipping_address: useSameAddress ? customerAddress : shippingAddress,
       };
 
       const response = await createOrder(orderData);
@@ -104,9 +133,51 @@ function CartPage({ cartItems, setCartItems, setTotalPrice, totalPrice }) {
           <input type="text" value={customerAddress} onChange={(e) => setCustomerAddress(e.target.value)} />
         </div>
         <div>
+          <label>Shipping Address:</label>
+          <input
+            type="text"
+            value={shippingAddress}
+            onChange={(e) => setShippingAddress(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Use same address for shipping:</label>
+          <input
+            type="checkbox"
+            checked={useSameAddress}
+            onChange={(e) => setUseSameAddress(e.target.checked)}
+          />
+        </div>
+        <div>
           <label>Credit Card Number:</label>
           <input type="text" value={creditCardNumber} onChange={CreditCardNumberChange} maxLength ="10" />
         </div>
+        <div>
+        <label>Credit Card Expiration:</label>
+        <input
+          type="text"
+          value={creditCardExp}
+          onChange={CreditCardExpChange}
+        />
+      </div>
+      <div>
+      <label>Credit Card CVV:</label>
+      <input
+        type="text"
+        value={creditCardCVV}
+        onChange={CreditCardCVVChange}
+        maxLength="3"
+      />
+      </div>
+      <div>
+        <label>Cardholder's Name:</label>
+        <input
+          type="text"
+          value={cardholderName}
+          onChange={CardholderNameChange}
+        />
+      </div>      
         {/* TODO SET PAYMENT AUTHORIZATION WITH DB  */}
         <Button
           color="success"
